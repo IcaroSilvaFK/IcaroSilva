e77e0913c1dc6925ec619c7acfdae4ee1e534ed5
Icaro Vieira <icarovsilva1@gmail.com>
2022-04-17 00:58:06 -0300

    feat images

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,33 @@ $("#menuMobile").click(() => {
   $("#menu").css("display", "flex");
   $("#nav__close").css("display", "block");
 });
-$("#menu").click(() => {
+$("#nav__close").click(() => {
   $("#menu").css("display", "none");
   $("#menuMobile").css("display", "block");
 });
+$("#link_sobre").click(() => {
+  $("#menu").css("display", "none");
+  $("#menuMobile").css("display", "block");
+});
+$("#link_projetos").click(() => {
+  $("#menu").css("display", "none");
+  $("#menuMobile").css("display", "block");
+});
+$("#link_contato").click(() => {
+  $("#menu").css("display", "none");
+  $("#menuMobile").css("display", "block");
+});
+
+$(document).ready($(window).bind("resize", CheckPx));
+
+function CheckPx() {
+  if ($(window).width() > 500) {
+    $("#menu").css("display", "block");
+    $("#menuMobile").css("display", "none");
+    $("#nav__close").css("display", "none");
+  }
+  if ($(window).width() <= 500) {
+    $("#menu").css("display", "none");
+    $("#menuMobile").css("display", "block");
+  }
+}
