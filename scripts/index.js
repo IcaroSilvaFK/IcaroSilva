61dcc28e9f096ae44ee842ef6b525e0e9d0bb46e
Icaro Vieira <icarovsilva1@gmail.com>
2022-04-25 13:30:17 -0300

    feat scroll animation

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+const $imgs = $("#tecnologies img");
+
 $("#menuMobile").click(() => {
   $("#menuMobile").css("display", "none");
   $("#menu").css("display", "flex");
@@ -33,3 +35,31 @@ function CheckPx() {
     $("#menuMobile").css("display", "block");
   }
 }
+
+ScrollReveal({ distance: "60px" });
+ScrollReveal().reveal("#text", { origin: "left", mobile: true });
+ScrollReveal().reveal("#imgPrincipal", { origin: "right", mobile: true });
+ScrollReveal().reveal("#projetos__container", {
+  origin: "left",
+  mobile: true,
+});
+
+ScrollReveal().reveal($imgs, {
+  origin: "bottom",
+  mobile: true,
+});
+
+// $imgs.each((index, value) => $(value).css("display", "none"));
+// $imgs.each((index, value) => {
+//   setTimeout(() => {
+//     $(value).css("display", "block");
+//     ScrollReveal().reveal(value, {
+//       opacity: 0,
+//     });
+//   }, index * 1000);
+// });
+
+ScrollReveal().reveal("#imgSobre", {
+  scale: 0.8,
+  mobile: true,
+});
